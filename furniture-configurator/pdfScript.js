0db735d2a07c66c5383779ea3df3051209e550bf
sarmad <sarmad.iict@gmail.com>
2021-12-07 11:38:24 +0500

    hide div

--- furniture-configurator/pdfScript.js
+++ furniture-configurator/pdfScript.js
@@ -1,9 +1,7 @@
 // alert('test');
 pdfBtn = document.querySelector("#generatePDF");
 
-const img1 = document.querySelector("#img1");
-const img2 = document.querySelector("#img2");
-const img3 = document.querySelector("#img3");
+
 
 if(pdfBtn){
 
@@ -15,13 +13,17 @@ if(pdfBtn){
 }
 
 function generatePDF() {
-
+  var img1 = document.querySelector("#img1");
+  var img2 = document.querySelector("#img2");
+  var img3 = document.querySelector("#img3");
     window.onmessage = (event) => {
 
+        console.log(event.data);
         if(event.data.substring(0,6)=='image1'){
-          var str1 = event.data.replace('image1','');
+
+          globalThis.str1 = event.data.replace('image1','');
           img1.setAttribute('src', str1);
-        //   console.log(str1);
+          console.log(img1);
         }
         else if(event.data.substring(0,6)=='image2'){
           // console.log('image 2 arahi');
@@ -34,6 +36,14 @@ function generatePDF() {
           var str3 = event.data.replace('image3','');
         //   console.log(str3);
           img3.setAttribute('src', str3);
+          setTimeout(function(){ 
+            var element = document.getElementById('pdfDiv');
+            element.style.display = 'block';
+            html2pdf().from(element).save();
+            
+          }, 1000);
+          
+          // element.style.display = 'none';
         }
       //    if (event.data.substring(0,14)=='app:image1') {
       //     let baseImg = event.data.replace('app:image1','test');
@@ -43,12 +53,13 @@ function generatePDF() {
       // Choose the element that our invoice is rendered in.
       // Choose the element and save the PDF for our user.
       // element.style.display = 'none';
-
-      var element = document.getElementById('pdfDiv');
-      element.style.display = 'block';
-      html2pdf().from(element).save();
       
-    };
+    }
 
+    
+    
+    
 
 }
+// console.log("ye he ", element)
+element.style.display = 'none'
